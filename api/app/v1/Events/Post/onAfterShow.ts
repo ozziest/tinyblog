9c560cdabcdd8c6ddc4627d81a999c5c9db580e9
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-09-29 12:25:07 +0200

    Fixed parent post view counts

--- api/app/v1/Events/Post/onAfterShow.ts
+++ api/app/v1/Events/Post/onAfterShow.ts
@@ -1,23 +1,34 @@
 import { IAfterShowContext } from "axe-api";
 
 export default async ({ database, item, req }: IAfterShowContext) => {
-  const postId = item.id;
   const userId = req.original.auth?.userId;
 
-  // Check the post view
-  const postView = await database
-    .table("post_views")
-    .where("user_id", userId)
-    .where("post_id", postId)
-    .first();
+  if (!userId) {
+    return;
+  }
 
-  // We should add the view if there is not any post view
-  if (!postView) {
-    await database.table("post_views").insert({
-      user_id: userId,
-      post_id: postId,
-      created_at: new Date(),
-      updated_at: new Date(),
-    });
+  const setAsViewed = async (postId: number) => {
+    // Check the post view
+    const postView = await database
+      .table("post_views")
+      .where("user_id", userId)
+      .where("post_id", postId)
+      .first();
+
+    // We should add the view if there is not any post view
+    if (!postView) {
+      await database.table("post_views").insert({
+        user_id: userId,
+        post_id: postId,
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+  };
+
+  await setAsViewed(item.id);
+
+  if (item.parent_id) {
+    await setAsViewed(item.parent_id);
   }
 };
