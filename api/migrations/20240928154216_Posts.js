48afc6c14ab1d30cfcc97f290d6b4d55dc1c88f5
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-09-30 19:27:39 +0200

    Added reshare_id column to the database

--- api/migrations/20240928154216_Posts.js
+++ api/migrations/20240928154216_Posts.js
@@ -1,6 +1,7 @@
 export const up = function (knex) {
   return knex.schema.createTable("posts", function (table) {
     table.increments();
+    table.integer("reshare_id").unsigned().nullable();
     table.integer("parent_id").unsigned().nullable();
     table.integer("user_id").unsigned().notNullable();
     table.string("content", 400).notNullable();
@@ -10,6 +11,7 @@ export const up = function (knex) {
     table.integer("stats_shares").notNullable().defaultTo(0);
     table.timestamps();
 
+    table.foreign("reshare_id").references("posts.id");
     table.foreign("parent_id").references("posts.id");
     table.foreign("user_id").references("users.id");
   });
