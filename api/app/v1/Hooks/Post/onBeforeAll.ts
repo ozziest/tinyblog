13a37b14fe3019c6e7f9f2b06578f025d292972b
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-10-06 11:27:49 +0200

    Added more cache and fixed feed queries

--- api/app/v1/Hooks/Post/onBeforeAll.ts
+++ api/app/v1/Hooks/Post/onBeforeAll.ts
@@ -1,9 +1,25 @@
-import { IAfterAllContext } from "axe-api";
+import { IBeforePaginateContext } from "axe-api";
+import FollowerService from "../../Services/FollowerService";
 import { FEED_ITEMS_PER_PAGE } from "../../../consts";
 
-export default async ({ query }: IAfterAllContext) => {
+export default async ({ query, req, res }: IBeforePaginateContext) => {
+  if (!req.original.auth) {
+    return res.status(403).json({ error: "Forbidden" });
+  }
+
   // Clients can not fetch all items. We have to apply a limit all the time.
   // But we can not allow clients decide the limit via frontend queries due to
   // performance concern.
   query.limit(FEED_ITEMS_PER_PAGE);
+
+  // Check if the client wanted to see the feed
+  const feed = req.query.get("feed");
+  if (feed === "true") {
+    // Who do I follow?
+    const followingIds = await FollowerService.getMyFollowingIds(
+      req.original.auth.userId
+    );
+    // Get me the only the posts of the people that I follow. But also, my posts
+    query.whereIn("user_id", [...followingIds, req.original.auth.userId]);
+  }
 };
--- api/app/v1/Services/FollowerService.ts
+++ api/app/v1/Services/FollowerService.ts
@@ -1,7 +1,17 @@
-import { IoCService } from "axe-api";
+import { IoCService, RedisAdaptor } from "axe-api";
 import { Knex } from "knex";
+import CacheService from "./CacheService";
 
 const getMyFollowings = async (authUserId: number) => {
+  const redis = await IoCService.use<RedisAdaptor>("Redis");
+  const key = CacheService.toKeys("FollowerService.getMyFollowings", {
+    authUserId,
+  });
+  const data = await redis.get(key);
+  if (data) {
+    return JSON.parse(data);
+  }
+
   const db = await IoCService.use<Knex>("Database");
 
   // Fetch my related likes
@@ -10,12 +20,14 @@ const getMyFollowings = async (authUserId: number) => {
     .select("id", "user_id")
     .where("follower_id", authUserId);
 
+  await redis.set(key, JSON.stringify(followings), 60 * 5);
+
   // The posts that I liked
   return followings;
 };
 
 const getMyFollowingIds = async (authUserId: number) => {
-  return (await getMyFollowings(authUserId)).map((item) => item.user_id);
+  return (await getMyFollowings(authUserId)).map((item: any) => item.user_id);
 };
 
 export default {
