fc1c726316cacda69c00027ba1b22bdb38aa2c6e
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-12 20:46:16 +0100

    Fixed hidden hashtag box issue

--- app/src/components/layout/Navigation.tsx
+++ app/src/components/layout/Navigation.tsx
@@ -74,31 +74,33 @@ const Navigation = () => {
         >
           #{dailyHashtag}
         </NavigationButton>
-        {hashtags.length > 0 && (
-          <>
-            <hr className="my-5 border-neutral-200" />
-            <h4 className="font-bold px-4 flex justify-between items-center">
-              Trends
-              <button
-                type="button"
-                className="bg-neutral-100 rounded-full p-1 transition hover:bg-neutral-200 duration-300"
-                onClick={() => emitter.emit("option-modal:on")}
-              >
-                <OptionsIcon size={20} />
-              </button>
-            </h4>
-            <div className="px-4 flex flex-col gap-2">
-              {hashtags.map((hashtag) => (
-                <NavigationLink
-                  key={hashtag}
-                  to={`/tags/${hashtag.replace("#", "")}`}
-                >
-                  {hashtag}
-                </NavigationLink>
-              ))}
+        <hr className="my-5 border-neutral-200" />
+        <h4 className="font-bold px-4 flex justify-between items-center">
+          Trends
+          <button
+            type="button"
+            className="bg-neutral-100 rounded-full p-1 transition hover:bg-neutral-200 duration-300"
+            onClick={() => emitter.emit("option-modal:on")}
+          >
+            <OptionsIcon size={20} />
+          </button>
+        </h4>
+        <div className="px-4 flex flex-col gap-2">
+          {hashtags.map((hashtag) => (
+            <NavigationLink
+              key={hashtag}
+              to={`/tags/${hashtag.replace("#", "")}`}
+            >
+              {hashtag}
+            </NavigationLink>
+          ))}
+          {hashtags.length === 0 && (
+            <div className="text-sm text-neutral-400 text-center py-5">
+              Hashtag trends will be displayed here. You can customize your
+              trending preferences by selecting multiple locations.
             </div>
-          </>
-        )}
+          )}
+        </div>
         <hr className="my-5 border-neutral-200" />
         <NavigationButton
           icon={<ProfileIcon size={24} />}
