655e35b3882bb47127adbf6085f41bd2870eeba4
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-13 22:38:33 +0100

    Added DB_PORT parameter

--- api/app/config.ts
+++ api/app/config.ts
@@ -81,6 +81,7 @@ const config: IApplicationConfig = {
     connection: {
       host: process.env.DB_HOST || "localhost",
       user: process.env.DB_USER || "user",
+      port: parseInt(process.env.DB_PORT || "3306"),
       password: process.env.DB_PASSWORD || "password",
       database: process.env.DB_DATABASE || "database",
     },
