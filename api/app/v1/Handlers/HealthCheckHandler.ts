ced3b3eb6a5e6355c89c59b9eaf79cc7a1dd5677
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-16 23:08:20 +0100

    Fixed hash

--- api/app/v1/Handlers/HealthCheckHandler.ts
+++ api/app/v1/Handlers/HealthCheckHandler.ts
@@ -1,6 +1,34 @@
 import { AxeRequest, AxeResponse } from "axe-api";
 import os from "os";
+import md5 from "md5";
+
+function getLocalIPAddress() {
+  const networkInterfaces = os.networkInterfaces();
+
+  for (const interfaceName in networkInterfaces) {
+    const interfaces = networkInterfaces[interfaceName];
+
+    if (!interfaces) {
+      break;
+    }
+
+    for (const iface of interfaces) {
+      // Check for IPv4 and exclude internal (loopback) addresses
+      if (iface.family === "IPv4" && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+
+  return null;
+}
+
+const ip = getLocalIPAddress();
 
 export default async (req: AxeRequest, res: AxeResponse) => {
-  res.status(200).json({ status: true, hostname: os.hostname() });
+  res.status(200).json({
+    status: true,
+    hostname: os.hostname(),
+    hash: ip ? md5(ip) : null,
+  });
 };
