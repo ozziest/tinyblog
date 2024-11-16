5037b84b1f7c18910b5996179b039fa2aff41db4
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-16 22:45:52 +0100

    Fixed

--- api/app/v1/Handlers/HealthCheckHandler.ts
+++ api/app/v1/Handlers/HealthCheckHandler.ts
@@ -1,5 +1,6 @@
 import { AxeRequest, AxeResponse } from "axe-api";
+import os from "os";
 
 export default async (req: AxeRequest, res: AxeResponse) => {
-  res.status(200).json({ status: true });
+  res.status(200).json({ status: true, hostname: os.hostname() });
 };
