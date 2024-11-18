739ea074c77d73b5ba32c2fabcf9c5a0165bb075
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-18 08:43:26 +0100

    Fix

--- api/app/v1/Handlers/HealthCheckHandler.ts
+++ api/app/v1/Handlers/HealthCheckHandler.ts
@@ -2,8 +2,13 @@ import { AxeRequest, AxeResponse } from "axe-api";
 import os from "os";
 
 export default async (req: AxeRequest, res: AxeResponse) => {
+  const xForwardedFor = req.original.headers["x-forwarded-for"];
+  const remoteAddress = req.original.socket.remoteAddress;
+
   res.status(200).json({
     status: true,
     hostname: os.hostname(),
+    xForwardedFor,
+    remoteAddress,
   });
 };
