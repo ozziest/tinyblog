b266ba5cd0709bfc9cbae7b2a00462c9308ed77e
mustafaer <mustafaerpro@gmail.com>
2024-11-19 00:20:46 +0300

    #7 user verify error changed to "We couldn't verify that you are a real user. Please try again later."

--- api/app/v1/Hooks/Registration/onBeforeInsert.ts
+++ api/app/v1/Hooks/Registration/onBeforeInsert.ts
@@ -35,7 +35,7 @@ export default async ({ req, formData, res }: IBeforeInsertContext) => {
     if (!isVerifiedByCF) {
       return res.status(400).json({
         error:
-          "We couldn't be sure your are a real user. Please try again later.",
+          "We couldn't verify that you are a real user. Please try again later.",
       });
     }
 
