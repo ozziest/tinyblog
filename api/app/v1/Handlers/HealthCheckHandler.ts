c863785d0534ce3425c2cf40cad5fc6d36685316
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-18 08:48:23 +0100

    Fix

--- api/app/v1/Handlers/HealthCheckHandler.ts
+++ api/app/v1/Handlers/HealthCheckHandler.ts
@@ -1,14 +1,27 @@
 import { AxeRequest, AxeResponse } from "axe-api";
 import os from "os";
 
+function getLocalNetworkIP() {
+  const interfaces = os.networkInterfaces();
+
+  for (const interfaceName in interfaces) {
+    for (const iface of interfaces[interfaceName] || []) {
+      // Check if it's an IPv4 address and not internal (not a loopback)
+      if (iface.family === "IPv4" && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+
+  return "Unable to determine local network IP";
+}
+
 export default async (req: AxeRequest, res: AxeResponse) => {
-  const xForwardedFor = req.original.headers["x-forwarded-for"];
-  const remoteAddress = req.original.socket.remoteAddress;
+  const localIP = getLocalNetworkIP();
 
   res.status(200).json({
     status: true,
     hostname: os.hostname(),
-    xForwardedFor,
-    remoteAddress,
+    localIP,
   });
 };
