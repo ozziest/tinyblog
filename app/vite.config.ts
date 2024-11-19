69a1a026676b02335352ad893060c69b4a212011
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-19 10:18:03 +0100

    Added sentry source maps

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,13 +1,25 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "tinyblog",
+      project: "api",
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve("src"), // Replace 'src' with your actual source folder
     },
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
