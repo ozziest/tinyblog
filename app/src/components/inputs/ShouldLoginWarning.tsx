6495798948ebb8f79e602472de3d41a4b95ad35f
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-20 16:26:34 +0100

    Fixed colors

--- app/src/components/inputs/ShouldLoginWarning.tsx
+++ app/src/components/inputs/ShouldLoginWarning.tsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 const ShouldLoginWarning = () => {
   return (
-    <div className="flex items-center justify-center bg-yellow-100 border border-yellow-300 p-4 text-center">
+    <div className="flex items-center justify-center bg-indigo-50 border border-indigo-200 p-4 text-center">
       <div>
-        <p className="text-yellow-800 font-semibold text-lg">Welcome! 😊</p>
-        <p className="text-yellow-700 mt-2">
+        <p className="text-indigo-800 font-semibold text-lg">Welcome! 😊</p>
+        <p className="text-indigo-700 mt-2">
           To share your thoughts, please{" "}
-          <Link to="/auth/login" className="font-bold">
+          <Link to="/auth/login" className="font-bold hover:underline">
             log in
           </Link>{" "}
           or{" "}
-          <Link to="/auth/register" className="font-bold">
+          <Link to="/auth/register" className="font-bold hover:underline">
             sign up
           </Link>
           .
