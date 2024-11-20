7b50e65fdb43e359a2f18e39dc098f310dccb171
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-20 14:13:38 +0100

    Fixed link issues

--- api/app/v1/Hooks/Post/onAfterShow.ts
+++ api/app/v1/Hooks/Post/onAfterShow.ts
@@ -11,37 +11,37 @@ export default async ({ req, item }: IAfterShowContext) => {
     (id) => id
   );
 
+  let myLikedPostIds: any[] = [];
+  let mySharedPostIds: any[] = [];
+
   if (userId) {
     // The posts that I liked
-    const myLikedPostIds = await PostService.getMyLikedPostIds(userId, postIds);
+    myLikedPostIds = await PostService.getMyLikedPostIds(userId, postIds);
 
     // The post ids that I already shared
-    const mySharedPostIds = await PostService.getMySharedPostIds(
-      userId,
-      postIds
-    );
-
-    // Set the like status for the pot
-    item.is_liked_by_you = myLikedPostIds.includes(item.id);
-
-    // Check if the post is already shared by yourself
-    item.is_shared_by_you = mySharedPostIds.includes(item.id);
-
-    // Set the like status for the parent if there is any
-    if (item.parent) {
-      item.parent.is_liked_by_you = myLikedPostIds.includes(item.parent.id);
-      item.parent.is_shared_by_you = mySharedPostIds.includes(item.parent.id);
-      item.parent.links = LinkService.mapLinks(item.parent?.links);
-    }
-
-    if (item.reshare) {
-      item.reshare.is_liked_by_you = myLikedPostIds.includes(item.reshare.id);
-      item.reshare.is_shared_by_you = mySharedPostIds.includes(item.reshare.id);
-      item.reshare.links = LinkService.mapLinks(item.reshare?.links);
-    }
-
-    item.links = LinkService.mapLinks(item?.links);
+    mySharedPostIds = await PostService.getMySharedPostIds(userId, postIds);
   }
+
+  // Set the like status for the pot
+  item.is_liked_by_you = myLikedPostIds.includes(item.id);
+
+  // Check if the post is already shared by yourself
+  item.is_shared_by_you = mySharedPostIds.includes(item.id);
+
+  // Set the like status for the parent if there is any
+  if (item.parent) {
+    item.parent.is_liked_by_you = myLikedPostIds.includes(item.parent.id);
+    item.parent.is_shared_by_you = mySharedPostIds.includes(item.parent.id);
+    item.parent.links = LinkService.mapLinks(item.parent?.links);
+  }
+
+  if (item.reshare) {
+    item.reshare.is_liked_by_you = myLikedPostIds.includes(item.reshare.id);
+    item.reshare.is_shared_by_you = mySharedPostIds.includes(item.reshare.id);
+    item.reshare.links = LinkService.mapLinks(item.reshare?.links);
+  }
+
+  item.links = LinkService.mapLinks(item?.links);
 
   // No need to use await
   PostService.incrementPostViews(postIds);
