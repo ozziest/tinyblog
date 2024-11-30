920ab720bc63be860cfeb9d13358cc010d9ebf1c
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-30 21:29:57 +0100

    Fixed daily hashtag

--- app/src/composables/useDailyLink.ts
+++ app/src/composables/useDailyLink.ts
@@ -1,11 +1,14 @@
 import { format } from "date-fns";
 import { DEFAULT_LANG } from "@/consts";
 import * as locales from "date-fns/locale";
+import useAuthStore from "@/stores/authStore";
 
 const useDailyLink = () => {
+  const authStore = useAuthStore();
+  const userLocale = authStore.state?.user?.location?.toLowerCase();
   const dailyHashtag = format(new Date(), "ddMMMMyyyy", {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    locale: (locales as any)[DEFAULT_LANG],
+    locale: (locales as any)[userLocale || DEFAULT_LANG],
   });
 
   const dailyLink = `/tags/${dailyHashtag}`;
